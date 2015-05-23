fd8b1ca1195000cf7eaa63ef124f7b52c8c7b630
Jan Schraepen <jan.schraepen@gmail.com>
2015-05-23 16:07:13 +0200

    added age calculation on detail

--- src/main/webapp/resources/js/general.js
+++ src/main/webapp/resources/js/general.js
@@ -1,11 +1,11 @@
-/* jQuery tabs */
+// jQuery tabs
 $(function() {
     var activeTab = $('#tabs').attr('active-tab');
     $('#tabs').tabs({active: activeTab});
 });
 
-/* _event button */
-$( document ).ready(function() {
+// _event button
+$(document).ready(function() {
 
     $('input[type="button"]').click(function (e) {
         e.preventDefault();
@@ -13,6 +13,17 @@ $( document ).ready(function() {
         var action = $(this).attr('action');
         $('input[name="_event"]').val(action);
         $(this).closest('form').submit();
+    });
+
+});
+
+// age culculation
+$(document).ready(function() {
+
+    $('form input[name="age"]').focusout(function(e) {
+        var birthYear = parseInt($(this).val());
+        var currentYear = new Date().getFullYear();
+        $('div.currentAge').html(currentYear - birthYear);
     });
 
 });
